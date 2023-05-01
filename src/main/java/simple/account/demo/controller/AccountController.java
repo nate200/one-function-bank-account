4f9b4d32deeb397718f1efb398e8dd9a00ab63ae
nate200 <newlifetyyyyy@hotmail.com>
2023-05-01 21:45:11 +0700

    init swagger

--- src/main/java/simple/account/demo/controller/AccountController.java
+++ src/main/java/simple/account/demo/controller/AccountController.java
@@ -1,8 +1,13 @@
 package simple.account.demo.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import simple.account.demo.exception.ApiError;
 import simple.account.demo.service.TransferManager;
 import simple.account.demo.model.Account;
 import simple.account.demo.model.Transaction;
@@ -14,12 +19,34 @@ public class AccountController {
     AccountService accountService;
 
     @PostMapping("create-account")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+                description = "save and return string",
+                content = @Content(schema = @Schema(type = "string"))),
+        @ApiResponse(responseCode = "400",
+                description = "input account has invalid field(s)",
+                content = @Content(schema = @Schema(implementation = ApiError.class))),
+        @ApiResponse(responseCode = "500",
+                description = "internal error",
+                content = @Content(schema = @Schema(implementation = ApiError.class)))
+    })
     public ResponseEntity<String> createAccount(@RequestBody Account account) {
         accountService.createAccountRequest(account);
         return ResponseEntity.ok("saved");
     }
 
     @GetMapping("getAccount/{accId}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "return an account with a given account id:{accId}",
+                    content = @Content(schema = @Schema(implementation = Account.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "no account found with a given account id:{accId}",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(responseCode = "500",
+                    description = "internal error",
+                    content = @Content(schema = @Schema(implementation = ApiError.class)))
+    })
     public Account getAccount(@PathVariable("accId") long accId) {
         return accountService.getAccountById(accId);
     }
--- src/main/java/simple/account/demo/controller/TransactionController.java
+++ src/main/java/simple/account/demo/controller/TransactionController.java
@@ -1,10 +1,17 @@
 package simple.account.demo.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import simple.account.demo.exception.ApiError;
+import simple.account.demo.model.Account;
 import simple.account.demo.model.Transaction;
 import simple.account.demo.service.TransferManager;
 
@@ -14,6 +21,20 @@ public class TransactionController {
     TransferManager transferManager;
 
     @PostMapping("transfer-with-in-app")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "return a message if everything went ok",
+                    content = @Content(schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "400",
+                    description = "Transaction request has field(s) with processable value such as null, wrong format, etc",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(responseCode = "422",
+                    description = "Transaction request is valid, but the api can't process the transaction. This can happen for example, when an account has invalid currency.",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(responseCode = "500",
+                    description = "internal error",
+                    content = @Content(schema = @Schema(implementation = ApiError.class)))
+    })
     public ResponseEntity<String> transferToWithInApp(@RequestBody Transaction transaction) throws Exception{
         transferManager.transferWithInApp(transaction);
         return ResponseEntity.ok("Transfer successful");
