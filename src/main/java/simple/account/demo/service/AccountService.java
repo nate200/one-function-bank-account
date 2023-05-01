fdac600427e857e854bd2c6bbcc70de1141e2103
nate200 <newlifetyyyyy@hotmail.com>
2023-05-01 14:12:42 +0700

    rework createAccountRequest checking

--- src/main/java/simple/account/demo/service/AccountService.java
+++ src/main/java/simple/account/demo/service/AccountService.java
@@ -1,29 +1,30 @@
 package simple.account.demo.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.ConstraintViolationException;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import simple.account.demo.exception.BadRequestParameterException;
 import simple.account.demo.model.Account;
 import simple.account.demo.repository.AccountRepository;
+import simple.account.demo.util.CurrencyUtil;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class AccountService {
     AccountRepository accountRepo;
 
     public void createAccountRequest(@NonNull Account account) {
-        Optional<Account> savedAcc = accountRepo.findByEmail(account.getEmail());
-        if(savedAcc.isPresent())
-            throw new BadRequestParameterException("Account with email[" + account.getEmail() + "] is already exist");
-
+        checkValidAccountBeforeSaving(account);
+        account.setId(null);
+        account.setEmail(account.getEmail().trim());
+        account.setCurrency(account.getCurrency().trim());
         accountRepo.save(account);
-        System.out.println("yes");
     }
 
     public Account getAccountById(long accId) {
@@ -35,7 +36,9 @@ public class AccountService {
         return getAccountById(accId).getCurrency();
     }
 
+    @Transactional
     public void changeTotal(@NonNull BigDecimal amount, long accId){
+        System.out.println("changeTotal: " + amount + " " + accId);
         int rowAffected = accountRepo.changeTotal(amount, accId);
         if(rowAffected == 0)
             throw throwAccountNotFound(accId);
@@ -45,6 +48,36 @@ public class AccountService {
         return new EntityNotFoundException("no account id:" + accId);
     }
 
+    private void checkValidAccountBeforeSaving(Account account) {
+        checkNullFieldsAccount(account);
+        checkCurrencySupport(account);
+        checkEmail(account);
+    }
+    private void checkNullFieldsAccount(Account account) {
+        try {
+            Objects.requireNonNull(account.getEmail(), "email");
+            Objects.requireNonNull(account.getTotal(), "total");
+            Objects.requireNonNull(account.getCurrency(), "currency");
+        }
+        catch (NullPointerException e) {
+            throw new BadRequestParameterException(e.getMessage() + " of the new account must not be null");
+        }
+    }
+    private void checkCurrencySupport(Account account){
+        try{ CurrencyUtil.getCurrencyFromString(account.getCurrency());}
+        catch (Exception e){
+            throw new BadRequestParameterException(e.getMessage());
+        }
+    }
+    private void checkEmail(Account account){
+        String email = account.getEmail().trim();
+        if(!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9+_.-]+\\.(.+)$"))
+            throw new BadRequestParameterException("Bad Email format:[" + account.getEmail() + "]");
+
+        Optional<Account> savedAcc = accountRepo.findByEmail(email);
+        if(savedAcc.isPresent())
+            throw new BadRequestParameterException("Account with email[" + account.getEmail() + "] is already exist");
+    }
 
     /*
     move to TransferManager
