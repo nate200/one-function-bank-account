874b3ad9e70ba278729cd012c85f8844cf1d3f60
nate200 <newlifetyyyyy@hotmail.com>
2023-05-01 22:25:36 +0700

    add more detail to Responses information

--- src/main/java/simple/account/demo/controller/AccountController.java
+++ src/main/java/simple/account/demo/controller/AccountController.java
@@ -1,10 +1,12 @@
 package simple.account.demo.controller;
 
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import simple.account.demo.exception.ApiError;
@@ -22,14 +24,19 @@ public class AccountController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200",
                 description = "save and return string",
-                content = @Content(schema = @Schema(type = "string"))),
+                content = @Content(examples = {@ExampleObject(value = "saved")},
+                        schema = @Schema(implementation = String.class),
+                        mediaType = MediaType.TEXT_PLAIN_VALUE)),
         @ApiResponse(responseCode = "400",
                 description = "input account has invalid field(s)",
-                content = @Content(schema = @Schema(implementation = ApiError.class))),
+                content = @Content(examples = {@ExampleObject(value = "{\"status\":\"BAD_REQUEST\",\"message\":\"total of the new account must not be null\",\"timestamp\":\"May 1, 2023, 10:07:39 PM\"}")},
+                        schema = @Schema(implementation = ApiError.class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE)),
         @ApiResponse(responseCode = "500",
                 description = "internal error",
-                content = @Content(schema = @Schema(implementation = ApiError.class)))
-    })
+                content = @Content(examples = {@ExampleObject(value = "{\"status\":\"INTERNAL_SERVER_ERROR\",\"message\":\"my bad ;(\",\"timestamp\":\"May 1, 2023, 10:19:31 PM\"}")},
+                        schema = @Schema(implementation = ApiError.class),
+                        mediaType = MediaType.APPLICATION_JSON_VALUE))})
     public ResponseEntity<String> createAccount(@RequestBody Account account) {
         accountService.createAccountRequest(account);
         return ResponseEntity.ok("saved");
@@ -39,14 +46,19 @@ public class AccountController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "return an account with a given account id:{accId}",
-                    content = @Content(schema = @Schema(implementation = Account.class))),
+                    content = @Content(examples = {@ExampleObject(value = "{\"id\":1,\"total\":10.00,\"currency\":\"THB\",\"email\":\"a@a.com\"}")},
+                            schema = @Schema(implementation = Account.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "404",
                     description = "no account found with a given account id:{accId}",
-                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+                    content = @Content(examples = {@ExampleObject(value = "{\"status\":\"NOT_FOUND\",\"message\":\"no account id:-1\",\"timestamp\":\"May 1, 2023, 10:15:52 PM\"}")},
+                            schema = @Schema(implementation = ApiError.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "500",
                     description = "internal error",
-                    content = @Content(schema = @Schema(implementation = ApiError.class)))
-    })
+                    content = @Content(examples = {@ExampleObject(value = "{\"status\":\"INTERNAL_SERVER_ERROR\",\"message\":\"my bad ;(\",\"timestamp\":\"May 1, 2023, 10:19:31 PM\"}")},
+                            schema = @Schema(implementation = ApiError.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE))})
     public Account getAccount(@PathVariable("accId") long accId) {
         return accountService.getAccountById(accId);
     }
--- src/main/java/simple/account/demo/controller/TransactionController.java
+++ src/main/java/simple/account/demo/controller/TransactionController.java
@@ -1,12 +1,13 @@
 package simple.account.demo.controller;
 
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,17 +25,24 @@ public class TransactionController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "return a message if everything went ok",
-                    content = @Content(schema = @Schema(type = "string"))),
+                    content = @Content(examples = {@ExampleObject(value = "Transfer successful")},
+                            schema = @Schema(implementation = String.class),
+                            mediaType = MediaType.TEXT_PLAIN_VALUE)),
             @ApiResponse(responseCode = "400",
-                    description = "Transaction request has field(s) with processable value such as null, wrong format, etc",
-                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+                    description = "Transaction request has field(s) with unprocessable value such as null, wrong format, etc",
+                    content = @Content(examples = {@ExampleObject(value = "{\"status\":\"BAD_REQUEST\",\"message\":\"The exchange amount must be decimal and greater than 0\",\"timestamp\":\"May 1, 2023, 10:22:50 PM\"}")},
+                            schema = @Schema(implementation = ApiError.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "422",
                     description = "Transaction request is valid, but the api can't process the transaction. This can happen for example, when an account has invalid currency.",
-                    content = @Content(schema = @Schema(implementation = ApiError.class))),
+                    content = @Content(examples = {@ExampleObject(value = "{\"status\":\"UNPROCESSABLE_ENTITY\",\"message\":\"Account id: 1 has invalid currency\",\"timestamp\":\"May 1, 2023, 10:22:10 PM\"}")},
+                            schema = @Schema(implementation = ApiError.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "500",
                     description = "internal error",
-                    content = @Content(schema = @Schema(implementation = ApiError.class)))
-    })
+                    content = @Content(examples = {@ExampleObject(value = "{\"status\":\"INTERNAL_SERVER_ERROR\",\"message\":\"my bad ;(\",\"timestamp\":\"May 1, 2023, 10:19:31 PM\"}")},
+                            schema = @Schema(implementation = ApiError.class),
+                            mediaType = MediaType.APPLICATION_JSON_VALUE))})
     public ResponseEntity<String> transferToWithInApp(@RequestBody Transaction transaction) throws Exception{
         transferManager.transferWithInApp(transaction);
         return ResponseEntity.ok("Transfer successful");
